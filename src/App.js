2b45f505118e6756c4ce6c3e90acf2fdb8be6b91
Abdul Wasiq <71366336+wroxtaaar@users.noreply.github.com>
2025-01-22 17:50:38 +0000

    fixed prop drillig issue

--- src/App.js
+++ src/App.js
@@ -55,22 +55,29 @@ export default function App() {
 
   return (
     <>
-      <Navbar movies={movies} />
+      <Navbar>
+        <Search />
+        <NumResults movies={movies} />
+      </Navbar>
 
-      <Main movies={movies} />
+      <Main>
+        <ListBox>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
 
- 
 
-function Navbar({ movies }) {
+
+function Navbar({ children }) {
 
   return (
     <nav className="nav-bar">
       <Logo />
-      <Search />
-      <NumResults movies={movies} />
+      {children}
     </nav>
   )
 }
@@ -111,13 +118,12 @@ function Search() {
 }
 
 
-function Main({ movies }) {
+function Main({ children }) {
 
 
   return (
     <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
+      {children}
     </main>
   )
 }
@@ -125,7 +131,7 @@ function Main({ movies }) {
 
 
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -136,7 +142,7 @@ function ListBox({ movies }) {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen1 && children}
     </div>
   )
 }
